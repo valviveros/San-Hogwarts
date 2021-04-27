a4c907bdc8a8328365ee7f32f0b22ac65f0f2c6c
valviveros <blkfthrs@gmail.com>
2021-04-27 15:16:22 -0500

    New menu where you can choose between being a Witch or a Wizard, added scene transitions, and the character stays selected.

---
+++ Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public GameObject[] characterList;
+    bool pressedWitch = false;
+    bool pressedWizard = false;
+    static public int index;
+    public GameObject canvas;
+    public GameObject fadeInpanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterList[0].gameObject.SetActive(true);
+        characterList[1].gameObject.SetActive(false);
+        pressedWitch = true;
+    }
+
+    public void onClick()
+    {
+        if (EventSystem.current.currentSelectedGameObject.name == "Witch")
+        {
+            characterList[0].gameObject.SetActive(true);
+            characterList[1].gameObject.SetActive(false);
+            pressedWitch = true;
+            pressedWizard = false;
+        }
+        if (EventSystem.current.currentSelectedGameObject.name == "Wizard")
+        {
+            characterList[0].gameObject.SetActive(false);
+            characterList[1].gameObject.SetActive(true);
+            pressedWitch = false;
+            pressedWizard = true;
+        }
+    }
+
+    private void Awake()
+    {
+        if (fadeInpanel != null)
+        {
+            GameObject panel = Instantiate(fadeInpanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
+
+    public void CallCouroutine(string sceneName)
+    {
+        StartCoroutine(SceneToLoad(sceneName));
+    }
+
+    public IEnumerator SceneToLoad(string sceneName)
+    {
+        if (pressedWitch)
+        {
+            index = 0;
+        }
+        else if (pressedWizard)
+        {
+            index = 1;
+        }
+        if (fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        canvas.SetActive(false);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterSelector characterSelector;
+    public GameObject[] characterList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnSceneLoaded();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnSceneLoaded()
+    {
+        if (CharacterSelector.index == 0)
+        {
+            characterList[0].SetActive(true);
+            characterList[1].SetActive(false);
+        }
+        else if (CharacterSelector.index == 1) {
+            characterList[0].SetActive(false);
+            characterList[1].SetActive(true);
+        }
+    }
+}
