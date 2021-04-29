0ae2094fc3b09dfa4fca55bd956693d3baa67748
Juan David Ruiz Lopez <49179416+JuanDa1997@users.noreply.github.com>
2021-04-29 10:58:24 -0500

    Inventario

---
+++ Assets/Scripts/InterationInventary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InterationInventary : MonoBehaviour
+{
+    public Button yourButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void elClick(){
+        // Debug.Log(gameObject.name);
+
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yourButton.onClick.AddListener(elClick);
+    }
+}
---
+++ Assets/Scripts/InventoryS.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InventoryS : MonoBehaviour
+{
+    public List<GameObject> Bag = new List<GameObject>();// lista para almacenar item
+    public GameObject inv;  //inventario
+    public bool Activar_inv; //desactivar o desactivar
+    public GameObject Selector;
+    public int ID;
+    // public Button yourButton;
+    public Vector2 position;
+    void Start()
+    {
+       
+        
+    }
+    void Update(){
+        
+    }
+    void OnTriggerEnter2D(Collider2D coll){ //detectar contacto con un objeto
+        if (coll.CompareTag("Item"))
+        {  
+            for (int i = 0; i < Bag.Count; i++) //tamaño de la lista
+            {
+                if (Bag[i].GetComponent<Image>().enabled == false) //componente imagen desactivado
+                {
+                    Bag[i].GetComponent<Image>().enabled = true; //si está desactivado entonces se activa
+                    Bag[i].GetComponent<Image>().sprite = coll.GetComponent<SpriteRenderer>().sprite; //la imagen es igual al obj colisionado
+                    break;
+                }
+            }
+        }
+    }
+    public void open()
+    {   
+        inv.SetActive(true);
+    }
+    public void close(){
+        inv.SetActive(false);
+    }
+
+    
+}
