d54058920fb1fd0368c78780552c9c10ff77fb1c
jcalzateb <54165082+jcalzateb@users.noreply.github.com>
2021-05-20 17:09:54 -0500

    implementacion del npc

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform objetivo;
+    public NavMeshAgent agente;
+
+     void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+        agente.updateRotation = false;
+        agente.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.destination = objetivo.position;
+        
+    }
+}
