cb1af3a7c1caa28b48473b13ae68d809b8aa5be2
Juan David Ruiz Lopez <49179416+JuanDa1997@users.noreply.github.com>
2021-05-30 19:08:12 -0500

    Implementación de poción en el inventario

---
+++ Assets/Scripts/ConfigScreen_lose.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ConfigScreen_lose : MonoBehaviour
+{
+    public GameObject ButtonStart;
+    public GameObject ButtonMenu;
+    public GameObject TitleLose;
+    // Start is called before the first frame update
+    public void OnGameObject(){ 
+        ButtonStart.SetActive(true);
+        ButtonMenu.SetActive(true);
+        TitleLose.SetActive(true);
+    }
+
+    public void Menu(){
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene("Potions");
+    }
+}
---
+++ Assets/Scripts/PotionConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionConfig : MonoBehaviour
+{
+    // Start is called before the first frame update   public GameObject jewerly;
+    public GameObject potion;
+
+    public AudioSource audioData;
+    // public AudioSource audioData2;
+
+    private InventoryS inventoryS;
+
+    public Sprite sprite;
+
+    void Start()
+    {
+        inventoryS = GameObject.FindObjectOfType<InventoryS>();
+        sprite = GetComponent<SpriteRenderer>().sprite;
+
+    }
+    void OnTriggerEnter2D(Collider2D collition){
+        if (collition.CompareTag("Player") && collition.isTrigger)
+        {
+            audioData.Play();
+            // audioData2.Play();
+            potion.SetActive(false);
+            inventoryS.updateInventory(sprite);
+        }
+    }
+}
