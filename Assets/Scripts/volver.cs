fab277a94148bb9438740358108adc12e0c55c6a
Juan David Ruiz Lopez <49179416+JuanDa1997@users.noreply.github.com>
2021-06-09 23:26:25 -0500

    Version Beta v2

---
+++ Assets/Scripts/volver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volver : MonoBehaviour
+{
+
+    public GameObject volverin;
+    public void buttonVolver(){
+        volverin.SetActive(false);
+    }
+}
